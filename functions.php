7c5027cf5516080832a3a3f2ff460696b1dd4c66
Timothy Kwan <tdkwan@gmail.com>
2016-11-08 19:31:57 -0800

    11-8-16 second commit: initializing plugins(css) with wordpress- doesn't work yet

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
 function afterschool_script_enqueue() {
-
+    wp_enqueue_style('customstyle', get_template_directory_uri() .'/css/afterschool-home.css', array(), '1.0.0', 'all');
 }
+
+add_action('wp_enqueue_scripts', 'afterschool_script_enqueue'); //wp_enqueue_scripts is the 'moment' when Wordpress enqueues all scripts, it is passed the function to run
--- header.php
+++ header.php
@@ -3,5 +3,6 @@
   <head>
     <meta charset="utf-8">
     <title>AFTER SCHOOL PROJECTS</title>
+    <?php wp_head(); ?> <!-- Wordpress function that says where to print all hooks and actions -->
   </head>
   <body>
