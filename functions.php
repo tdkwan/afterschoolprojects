f619287498f97b7091ae71aa97fc3a9ec767ef92
Timothy Kwan <tdkwan@gmail.com>
2016-11-13 02:00:11 -0800

    11-13-16:commit 2, implementing bootstrap and overriding jquery scripts works, (tether not working because we don't need tooltips yet)

--- functions.php
+++ functions.php
@@ -2,6 +2,18 @@
 
 function afterschool_script_enqueue() {
     wp_enqueue_style('customstyle', get_template_directory_uri() .'/css/afterschool-home.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('bootstrapcss', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.0.0', 'all');
+    wp_enqueue_script('bootstrapjs', get_template_directory_uri() .'/js/bootstrap.min.js', array(), '4.0.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'afterschool_script_enqueue'); //wp_enqueue_scripts is the 'moment' when Wordpress enqueues all scripts, it is passed the function to run
+
+function modify_jquery() {
+    if (!is_admin()) {
+        wp_deregister_script('jquery');
+        wp_register_script('jquery', get_template_directory_uri() . '/js/jquery-3.1.0.min.js', '3.1.0', false);
+        wp_enqueue_script('jquery');
+    }
+}
+
+add_action('init', 'modify_jquery');
