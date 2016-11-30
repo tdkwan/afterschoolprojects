80ddcda4d495aaec86f56a15791aa285d0651e6c
Timothy Kwan <tdkwan@gmail.com>
2016-11-30 01:44:19 -0800

    11-30-16 commit:implemented small sphere menu, messed with css, still working post page coming together, need to figure out wordpress post attachment formatting

--- functions.php
+++ functions.php
@@ -1,27 +1,20 @@
 <?php
 
 function afterschool_script_enqueue() {
-    wp_enqueue_style('customstyle', get_template_directory_uri() .'/css/style.css', array(), '1.0.0', 'all');
     wp_enqueue_style('bootstrapcss', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.0.0', 'all');
+    wp_enqueue_style('customstyle', get_template_directory_uri() .'/css/afterschool-home.css', array(), '1.0.0', 'all');
+    wp_enqueue_script('jquery');
     wp_enqueue_script('bootstrapjs', get_template_directory_uri() .'/js/bootstrap.min.js', array(), '4.0.0', true);
+
 }
 
 add_action('wp_enqueue_scripts', 'afterschool_script_enqueue'); //wp_enqueue_scripts is the 'moment' when Wordpress enqueues all scripts, it is passed the function to run
 
-function modify_jquery() {
-    if (!is_admin()) {
-        wp_deregister_script('jquery');
-        wp_register_script('jquery', get_template_directory_uri() . '/js/jquery-3.1.0.min.js', '3.1.0', false);
-        wp_enqueue_script('jquery');
-    }
-}
-
-add_action('init', 'modify_jquery');
 
 function afterschool_theme_setup() {
     add_theme_support('menus');
 
-    register_nav_menu('welcome-menu', 'Home Page: Jumbotron Welcome Menu');
+    register_nav_menu('header-menu', 'Header Menu: Home Base');
     register_nav_menu('primary-navigation', 'Primary Navigation Menu: Spheres');
 }
 
@@ -30,3 +23,4 @@ add_action('after_setup_theme', 'afterschool_theme_setup');
 add_theme_support('custom-background');
 add_theme_support('custom-header');
 add_theme_support('post-thumbnails');
+add_theme_support('post-formats',array('aside','image','gallery'));
--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>AFTER SCHOOL PROJECTS</title>
-    <link href="https://fonts.googleapis.com/css?family=Amiri:700|Contrail+One" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Amiri:700|Contrail+One|Roboto" rel="stylesheet">
     <?php wp_head(); ?> <!-- Wordpress function that says where to print all hooks and actions -->
   </head>
 
@@ -15,4 +15,3 @@
     endif;
   ?>
   <body <?php body_class($afterschool_classes)?>>
-    <?php wp_nav_menu(array('theme_location'=>'primary-navigation')); ?>
